9be3e7d1ed6cfc96a709686cc5d4f3e62ace8007
alirazahb <aliraza.ar7785@gmail.com>
2023-05-26 10:38:56 +0500

    adding login screen

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 const LoginScreen = () => {
-  return (
-    <View>
-        <Text> LoginScreen </Text>
-        
-    </View>
-  )
-}
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-export default LoginScreen
+  const handleLogin = () => {
+    // Handle login logic
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        secureTextEntry
+        onChangeText={setPassword}
+        style={styles.input}
+      />
+      <Button mode="contained" onPress={handleLogin} style={styles.button}>
+        Login
+      </Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 16,
+  },
+});
+
+export default LoginScreen;
